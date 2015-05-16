387d9731cc883cf605c896193b39409e25e990dc
katspaugh <ivan@mycs.com>
2015-05-17 02:00:39 +0300

    reverse mapping for ref request

--- coffee.js
+++ coffee.js
@@ -3,82 +3,67 @@ var coffee = require('coffee-script');
 var SourceMapConsumer = require('source-map').SourceMapConsumer;
 
 
-var toLineCol = function (offset, lines) {
-    var position = {
-        line: 1,
-        column: 0
-    };
-    var len = lines.length;
-    var sumOffset = 0;
-    for (var i = 0; i < len; i++) {
-        var lineLen = lines[i].length + 1;
-        if (sumOffset + lineLen > offset) {
-            position.line = i + 1;
-            position.column = offset - sumOffset;
-            break;
-        }
-        sumOffset += lineLen;
+var compile = function (fileName, text) {
+    try {
+        var compiled = coffee.compile(text, {
+            bare: true,
+            sourceMap: true,
+            header: false,
+
+            sourceFiles: [ fileName ]
+        });
+    } catch (e) {
+        return;
     }
-    return position;
+
+    var map = JSON.parse(compiled.v3SourceMap);
+
+    compiled.sourceMapConsumer = new SourceMapConsumer(map);
+    compiled.source = text;
+    compiled.name = fileName;
+
+    return compiled;
 };
 
-var toOffset = function (position, lines) {
-    var offset = 0;
-    for (var i = 0; i < position.line; i++) {
-        offset += lines[i].length;
-    }
-    offset += position.column;
+var toLineCol = function (offset, text) {
+    var prevText = text.slice(0, offset);
+    var lines = prevText.split('\n');
+    var line = lines.length;
+    var column = lines[line - 1].length;
+    return {
+        line: line,
+        column: column
+    };
+};
+
+var toOffset = function (lineCol, text) {
+    var allLines = text.split('\n');
+    var prevText = allLines.slice(0, lineCol.line - 1).join('\n');
+    var offset = prevText.length + lineCol.column - 1;
     return offset;
 };
 
-var setOriginalPositions = function (ast, compiled) {
-    var smc = compiled.smc;
-    var jsLines = compiled.js.split('\n');
-    var sourceLines = compiled.source.split('\n');
-
-    ast.body.forEach(function (node) {
-        node.start = toOffset(
-            smc.originalPositionFor(toLineCol(node.start, jsLines)),
-            sourceLines
-        );
-        node.end = toOffset(
-            smc.originalPositionFor(toLineCol(node.end, jsLines)),
-            sourceLines
-        );
+var getSourcePosition = function (offset, compiled) {
+    var lineCol = toLineCol(offset, compiled.js);
+    var origLineCol = compiled.sourceMapConsumer.originalPositionFor({
+        line: lineCol.line,
+        column: lineCol.column
     });
+    var origOffset = toOffset(origLineCol, compiled.source);
+
+    return origOffset;
 };
 
-var getCompiledPosition = function (offset, compiled) {
-    var position = toLineCol(offset, compiled.source.split('\n'));
-    var jsPosition = compiled.smc.generatedPositionFor({
+var getGeneratedPosition = function (offset, compiled) {
+    var lineCol = toLineCol(offset, compiled.source);
+    var genLineCol = compiled.sourceMapConsumer.generatedPositionFor({
         source: compiled.name,
-        line: position.line,
-        column: position.column
+        line: lineCol.line,
+        column: lineCol.column
     });
-    return toOffset(jsPosition, compiled.js.split('\n'));
-};
+    var genOffset = toOffset(genLineCol, compiled.js);
 
-var modifyQuery = function (query, compiled) {
-    query.end = getCompiledPosition(query.end, compiled);
-};
-
-var compile = function (source) {
-    return coffee.compile(source, {
-        bare: true,
-        sourceMap: true
-    });
-};
-
-var saveCompiled = function (server, name, source) {
-    var answer = compile(source);
-    var sourceMap = JSON.parse(answer.v3SourceMap);
-    sourceMap.sources = [ name ];
-    return server._coffee.files[name] = {
-        name: name,
-        source: source,
-        js: answer.js,
-        smc: new SourceMapConsumer(sourceMap)
-    };
+    return genOffset;
 };
 
 var isCoffee = function (fileName) {
@@ -86,61 +71,101 @@ var isCoffee = function (fileName) {
 };
 
 var initServer = function (server) {
-    return server._coffee = {
-        files: {}
-    };
+    return server._coffee = {};
 };
 
 
 tern.registerPlugin('coffee', function (server) {
     // Overwrite options.getFile
-    server.options.getFile = (function (getFile) {
-        return function (name, callback) {
-            getFile.call(this, name, function (error, data) {
-                if (!error && isCoffee(name)) {
-                    callback(null, saveCompiled(server, name, data).js);
-                } else {
-                    callback(error, data);
-                }
-            });
-        };
-    }(server.options.getFile));
+    var origGetFile = server.options.getFile;
 
+    server.options.getFile = function (name, callback) {
+        return origGetFile.call(this, name, function (err, data) {
+            if (err || !isCoffee(name)) {
+                return callback(err, data);
+            }
+
+            var compiled = compile(name, data);
+
+            if (compiled) {
+                server._coffee[name] = compiled;
+
+                callback(null, compiled.js);
+            } else {
+                callback('Cannot compile CoffeeScript');
+            }
+        });
+    };
 
     // Overwrite request
-    server.request = (function (request) {
-        return function (doc, callback) {
-            if (isCoffee(doc.query.file)) {
-                return request.call(server, doc, callback);
-            }
+    var origRequest = server.request;
 
-            if (doc.files) {
-                // TODO
-            }
+    server.request = function (doc, callback) {
+        var doRequest = function (err) {
+            if (err) {
+                callback(err);
+            } else {
+                origRequest.call(server, doc, function (err, data) {
+                    console.log(data);
 
-            server.options.getFile(doc.query.file, function () {
-                var compiled = server._coffee.files[doc.query.file];
-                modifyQuery(doc.query, compiled);
-                request.call(server, doc, callback);
-            });
+                    if (!err) {
+                        var compiled = doc.query && server._coffee[doc.query.file];
+
+                        if (data.refs) {
+                            data.refs.forEach(function (ref) {
+                                ref.start = getSourcePosition(ref.start, compiled);
+                                ref.end = getSourcePosition(ref.end, compiled);
+                            });
+                        }
+                    }
+
+                    callback(err, data);
+                });
+            }
         };
-    }(server.request));
 
+        if (doc.files) {
+            doc.files.forEach(function (file) {
+                if (isCoffee(file.name)) {
+                    var compiled = compile(file.name, file.text);
 
+                    if (compiled) {
+                        server._coffee[file.name] = compiled;
+
+                        file.text = compiled.js;
+                    }
+                }
+            });
+        }
+
+        if (doc.query && isCoffee(doc.query.file) && doc.query.end) {
+            var compiled = server._coffee[doc.query.file];
+
+            if (compiled) {
+                doc.query.end = getGeneratedPosition(doc.query.end, compiled);
+
+                doRequest();
+            } else {
+                server.options.getFile(doc.query.file, function (err, data) {
+                    if (err) {
+                        doRequest(err);
+                    } else {
+                        var compiled = server._coffee[doc.query.file];
+                        doc.query.end = getGeneratedPosition(doc.query.end, compiled);
+                    }
+                });
+            }
+        } else {
+            doRequest();
+        }
+    };
+
+    // Init
     initServer(server);
 
     server.on('reset', function () {
         initServer(server);
     });
 
-    return {
-        passes: {
-            postInfer: function (ast) {
-                var compiled = server._coffee.files[server._coffee.currentFile];
-                if (compiled) {
-                    setOriginalPositions(ast, compiled);
-                }
-            }
-        }
-    };
+    return {};
 });
